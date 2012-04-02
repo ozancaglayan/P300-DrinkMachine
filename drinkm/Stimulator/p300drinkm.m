d806bd2d153db048c2e898defaa0edb90f158539
Ozan Caglayan <ozancag@gmail.com>
2012-04-02 17:07:52 +0300

    Update

--- drinkm/Stimulator/p300drinkm.m
+++ drinkm/Stimulator/p300drinkm.m
@@ -3,35 +3,49 @@ function p300drinkm(times, runs, ftime, noftime, samplerate)
 % Store drink names
 drinks = {'Water', 'Coffee', 'Tea', 'Soda', 'Beer'};
 
+% Used to trigger the Analyzer
+mynoise(1,:) = MakeBeep(1000, 0.1, 8192);
+
+% Stimulus data
+stimulus = zeros(runs, times * length(drinks));
+
+% Cues for timestamps
+cues = zeros(runs, times * length(drinks));
+
 % Load auditory stimuli
 load('data/soundz.mat', 'soundz');
 
 GetSecs;
 
-% Choosing the display with the highest dislay number is
-% a best guess about where you want the stimulus displayed.
-screens = Screen('Screens');
-screenNumber = max(screens);
-
 % Colors
-bcolor  = [0 0 0];
 tcolor  = [128 128 128];
 
 % Open window
-w = Screen('OpenWindow', screenNumber);
-Screen('FillRect', w, bcolor);
+w = Screen('OpenWindow', 0, [0 0 0]);
+Screen('Preference', 'TextAntialiasing', 0);
+Screen('Preference', 'Verbosity', 5);
+HideCursor;
+Priority(MaxPriority(w));
+
+% The slack is just to place the stimulus presentation deadline
+% in the middle of a video refresh cycle, so it has some "slack" to the
+% previous and next frame boundary.
+slack = Screen('GetFlipInterval', w) / 2;
 
 % Load images and create PTB textures
-texback = Screen('MakeTexture', w, double(imread('data/drinksback', 'JPG')));
-tex1 = Screen('MakeTexture', w, double(imread('data/drinks1', 'JPG')));
-tex2 = Screen('MakeTexture', w, double(imread('data/drinks2', 'JPG')));
-tex3 = Screen('MakeTexture', w, double(imread('data/drinks3', 'JPG')));
-tex4 = Screen('MakeTexture', w, double(imread('data/drinks4', 'JPG')));
-tex5 = Screen('MakeTexture', w, double(imread('data/drinks5', 'JPG')));
-textures = [tex1, tex2, tex3, tex4, tex5];
+textures = zeros(6, 1);
 
-Screen('DrawText', w, 'Connecting to other computer', 100, 100, tcolor);
-Screen('Flip', w);
+for i=1:6
+    % 6th one is the default background
+    textures(i) = Screen('MakeTexture', w, imread(strcat('data/drinks', num2str(i), '.jpg')));
+end
+
+% Preload textures into VRAM if possible
+Screen('PreloadTextures', w, textures);
+
+% Draw background
+Screen('DrawTexture', w, textures(6));
+Screen('Flip', w, [], 1);
 
 ipA = '10.1.2.142';
 portA  = 9090;
@@ -47,14 +61,9 @@ fopen(udpA);
 fopen(udpA2);
 fprintf('ports opened\n');
 
-Screen('DrawText', w, 'ports opened', 100, 100, tcolor);
-Screen('Flip',w);
-
 handshake(udpA, udpA2);
 
 fprintf('sending data');
-Screen('DrawText', w, 'Sending initial parameters', 100, 100, tcolor);
-Screen('Flip', w);
 
 fprintf(udpA, num2str(times));
 fprintf(udpA, num2str(runs));
@@ -63,58 +72,57 @@ fprintf(udpA, num2str(noftime));
 fprintf(udpA, num2str(samplerate));
 
 fprintf('data sent');
-Screen('DrawText', w, 'initial values sent', 100, 100, tcolor);
-Screen('Flip', w);
+Screen('DrawText', w, 'Communication: OK.', 100, 100, tcolor);
+Screen('Flip', w, [], 1);
 
-% Used to trigger the Analyzer
-mynoise(1,:) = MakeBeep(1000, 0.1, 8192);
-
-% Stimulus data
-stimulus = zeros(runs, times * length(drinks));
-
-% Cues for timestamps
-cues = zeros(runs, times * length(drinks));
+% Pre-generate permutations
+for r = 1:runs
+    for t = 1:times
+        perm = randperm(length(drinks));
+        bound = length(drinks) * t;
+        stimulus(r, bound-4:bound) = perm;
+    end
+end
 
 for k = 1:runs
-    count = 0;
-
     % Prepare the subject
-    Screen('DrawText', w, '3', 100, 100, tcolor);
-    Screen('Flip', w);
+    Screen('DrawText', w, '3', 100, 130, tcolor);
+    Screen('Flip', w, [], 1);
     WaitSecs(1);
-    Screen('DrawText', w, '2', 100, 100, tcolor);
-    Screen('Flip', w);
+    Screen('DrawText', w, '2', 120, 130, tcolor);
+    Screen('Flip', w, [], 1);
     WaitSecs(1);
-    Screen('DrawText', w, '1', 100, 100, tcolor);
-    Screen('Flip', w);
-    
-    % Send the trigger
+    Screen('DrawText', w, '1', 140, 130, tcolor);
+    Screen('Flip', w, [], 1);
+    WaitSecs(1);
+
+    % Play the sound which will start EEG recording immediately
     Snd('Play', mynoise, 8192, 16);
+    %WaitSecs(0.2);
 
-    start = GetSecs;
-    tic;
-    %WaitSecs(1);
+    % Record the start time
+    last_onset = GetSecs();
 
-    for j = 1:times
+    for stim_count = 1:times * length(drinks)
+        target_time = last_onset + noftime - slack;
+        stimuli = stimulus(k, stim_count);
 
-        for randnum = randperm(5)
-            Screen('DrawTexture', w, textures(randnum));
-            count = count + 1;
-            cues(k, count) = int32(samplerate * toc);
-            stimulus(k, count) = randnum;
-            [VBLTimestamp StimulusOnsetTime FlipTimestamp Missed Beampos] = Screen('Flip', w);
-            sound(soundz(randnum, :)*0.7, 16000);
-            WaitSecs(ftime);
-            
-            % Draw the background
-            Screen('DrawTexture', w, texback);
-            Screen('Flip', w);
-            WaitSecs(noftime);
-        end
+        Screen('DrawTexture', w, textures(stimuli));
+
+        % Record the flip timestamp
+        cues(k, stim_count) = Screen('Flip', w, target_time);
+        
+        % Play sound asynchronously
+        sound(soundz(stimuli, :), 16000);
+
+        % Draw the background and wait noftime
+        Screen('DrawTexture', w, textures(6));
+        last_onset = Screen('Flip', w, cues(k, stim_count) + ftime - slack);
     end
-    
-    stop = GetSecs;
-    fprintf('Finished in %f seconds.\n', stop - start);
+
+    % Round (and make sure that none of the cues == 0)
+    cues(k, :) = int32(ceil(cues(k, :) * sample_rate));
+    % cues(k, 1) = cues(k, 1) + 1;
     
     assignin('base', 'stimulus', stimulus);
     assignin('base', 'cues', cues);
@@ -141,12 +149,13 @@ for k = 1:runs
 
     % Wait 1 second
     WaitSecs(1);
-
 end
 
 KbWait;
+Priority(0);
 
-fclose('all');
+fclose(udpA);
+fclose(udpA2);
 
 % This "catch" section executes in case of an error in the "try" section
 % above.  Importantly, it closes the onscreen window if it's open.
